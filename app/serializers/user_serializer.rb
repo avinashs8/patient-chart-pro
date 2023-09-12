c131c1d85d802a16eb116516aab347088c8a1f1b
avinash singh <avinash8.as@gmail.com>
2023-09-12 18:03:29 -0400

    fix serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :name, :email, :specialization, :address, :phone_number
+  attributes :id, :name, :email, :specialization, :address, :phone_number
 end
