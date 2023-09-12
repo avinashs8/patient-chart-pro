d79c7213441cd7a85547de4b302c22df7c4c9449
avinash singh <avinash8.as@gmail.com>
2023-09-12 17:27:00 -0400

    finish session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+    #signin
+    def create
+        user = User.find_by(name: params[:name])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id 
+            render json: user, status: :ok
+        else
+            render json: { error: "Invalid username or password" }, status: :unauthorized
+        end
+    end
+
+    #signout
+    def destroy
+        session.clear
+    end
+end
