febe9131d24bc3a8c2f16a74039c316dce79e8c6
avinash singh <avinash8.as@gmail.com>
2023-09-12 17:12:47 -0400

    add login/out functions to context

--- client/src/context/User.js
+++ client/src/context/User.js
@@ -6,6 +6,21 @@ const UserContext = React.createContext()
 function UserProvider({ children }) {
 
     const [ user, setUser ] = useState(null)
+    const [ loggedIn, setloggedIn ] = useState(false)
+
+    const signup = (user) =>{
+      setUser(user)
+      setloggedIn(true)
+    }
+
+    const login = (user) => {
+      setUser(user)
+      setloggedIn(true)
+    }
+
+    const logout = () => {
+      setUser(null)
+    }
 
   return (
     <UserContext.Provider value={{ user, setUser }}>{ children }</UserContext.Provider>
